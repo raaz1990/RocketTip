edc29ed764006acc505e908af08e3e6ae56aaa2d
raaz1990 <raaj.choubisa1990@gmail.com>
2024-12-03 08:16:36 +0530

    custom popup box added to paytips page maui

--- RocketTip/Pages/PayTipPage.xaml.cs
+++ RocketTip/Pages/PayTipPage.xaml.cs
@@ -1,9 +1,49 @@
-namespace RocketTip.Pages;
+using Microsoft.Maui.Controls;
 
-public partial class PayTipPage : ContentPage
+namespace RocketTip.Pages
 {
-	public PayTipPage()
-	{
-		InitializeComponent();
-	}
+    public partial class PayTipPage : ContentPage
+    {
+        public PayTipPage()
+        {
+            InitializeComponent();
+        }
+
+        // Method to Show the Popup
+        private void ShowPopup(string paymentMethod)
+        {
+            // Update popup content based on the payment method
+            switch (paymentMethod)
+            {
+                case "ipay":
+                    PopupImage.Source = "rockettipslogowithroce.png";
+                    PopupLabel.Text = "You selected iPay. Your Tip Payment Was Successful.";
+                    break;
+                case "gpay":
+                    PopupImage.Source = "rockettipslogowithroce.png";
+                    PopupLabel.Text = "You selected GPay. Your Tip Payment Was Successful.";
+                    break;
+                default:
+                    PopupImage.Source = "rockettipslogowithroce.png";
+                    PopupLabel.Text = "Your Tip Payment Was Successful.";
+                    break;
+            }
+
+            // Show the popup
+            PopupView.IsVisible = true;
+        }
+
+        // Method to Hide the Popup
+        private void ClosePopup(object sender, EventArgs e)
+        {
+            PopupView.IsVisible = false;
+        }
+
+        // Handle Payment Method Tap
+        private void OnPaymentMethodTapped(object sender, TappedEventArgs e)
+        {
+            string paymentMethod = e.Parameter?.ToString();
+            ShowPopup(paymentMethod);
+        }
+    }
 }
